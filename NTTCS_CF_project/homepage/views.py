dd14cc0b58e211982a8fd486a0306f0fb19ba3ed
eloyrj <48245099+eloyrj@users.noreply.github.com>
2023-06-13 14:54:18 +0200

    preparado para trabajo y  creacion del login
    se han preparado todas las paginas necesarias para el proyecto a demas de tener finalizada la pagina del login

--- NTTCS_CF_project/homepage/views.py
+++ NTTCS_CF_project/homepage/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'homepage/index.html')
+    user = request.POST.get('usuario')
+    passw = request.POST.get('pass')
+
+    if user == 'Eloy' and passw == '1234':
+        return render(request, 'homepage/menu.html')
+
+    return render(request, 'homepage/index.html')
+
+def assessment(request):
+    return render(request, 'homepage/assessment.html')
+
+def assessmentselect(request):
+    return render(request, 'homepage/assessmentselect.html')
+
+def Exportaciones(request):
+    return render(request, 'homepage/Exportaciones.html')
+
+def informes(request):
+    return render(request, 'homepage/informes.html')
+
+def Mantenimiento(request):
+    return render(request, 'homepage/Mantenimiento.html')
+
+def menu(request):
+    return render(request, 'homepage/menu.html')
