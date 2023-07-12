20b032a7c7433d25f8304f4793041b606f5eba7e
kaynnerv <kaynnerv@gmail.com>
2023-07-12 11:43:33 +0200

    cambios vistas1

--- NTTCS_CF_project/homepage/views.py
+++ NTTCS_CF_project/homepage/views.py
@@ -247,6 +247,8 @@ class MantenimientoNivelMadurez(TemplateView):
 
         return redirect('mantenimientoNivelMadurez')
 
+# Clase para la pagina de inicio de sesion
+
 # Clase para la pagina de menu
 class menu(TemplateView):
     template_name = "homepage/menu.html"
