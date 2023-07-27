9b44a6599560733c1c85e91c199e94fb6bd2441e
eloyrj <48245099+eloyrj@users.noreply.github.com>
2023-07-27 14:32:59 +0200

    mapeo marcos, ahora añadido

--- NTTCS_CF_project/NTTCS_CF_project/urls.py
+++ NTTCS_CF_project/NTTCS_CF_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
 ]
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
