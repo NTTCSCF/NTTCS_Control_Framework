2feb5147954ed6d45e542ab0e39e8c76a0ad67e9
eloyrj <48245099+eloyrj@users.noreply.github.com>
2023-10-10 09:33:39 +0200

    tablas en word

--- NTTCS_CF_project/staticfiles/js/scripts.js
+++ NTTCS_CF_project/staticfiles/js/scripts.js
@@ -1,63 +1,63 @@
 function doSearch(){
-            const tableReg = document.getElementById('data');
-            const searchText = document.getElementById('busqueda').value.toLowerCase();
-            let total = 0;
+    const tableReg = document.getElementById('data');
+    const searchText = document.getElementById('busqueda').value.toLowerCase();
+    let total = 0;
 
-            // Recorremos todas las filas con contenido de la tabla
-            for (let i = 1; i < tableReg.rows.length; i++) {
-                // Si el td tiene la clase "noSearch" no se busca en su cntenido
-                if (tableReg.rows[i].classList.contains("noSearch")) {
-                    continue;
-                }
-                let found = false;
-                const cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
-                // Recorremos todas las celdas
-                for (let j = 0; j < cellsOfRow.length && !found; j++) {
-                    const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
-                    // Buscamos el texto en el contenido de la celda
-                    if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
-                        found = true;
-                        total++;
-                    }
-                }
-                if (found) {
-                    tableReg.rows[i].style.display = '';
-                } else {
-                    // si no ha encontrado ninguna coincidencia, esconde la
-                    // fila de la tabla
-                    tableReg.rows[i].style.display = 'none';
-                }
-            }
-
-            // mostramos las coincidencias
-            const lastTR=tableReg.rows[tableReg.rows.length-1];
-            const td=lastTR.querySelector("td");
-            lastTR.classList.remove("hide", "red");
-            if (searchText == "") {
-                lastTR.classList.add("hide");
+    // Recorremos todas las filas con contenido de la tabla
+    for (let i = 1; i < tableReg.rows.length; i++) {
+        // Si el td tiene la clase "noSearch" no se busca en su cntenido
+        if (tableReg.rows[i].classList.contains("noSearch")) {
+            continue;
+        }
+        let found = false;
+        const cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+        // Recorremos todas las celdas
+        for (let j = 0; j < cellsOfRow.length && !found; j++) {
+            const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+            // Buscamos el texto en el contenido de la celda
+            if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
+                found = true;
+                total++;
             }
         }
+        if (found) {
+            tableReg.rows[i].style.display = '';
+        } else {
+            // si no ha encontrado ninguna coincidencia, esconde la
+            // fila de la tabla
+            tableReg.rows[i].style.display = 'none';
+        }
+    }
+
+    // mostramos las coincidencias
+    const lastTR=tableReg.rows[tableReg.rows.length-1];
+    const td=lastTR.querySelector("td");
+    lastTR.classList.remove("hide", "red");
+    if (searchText == "") {
+        lastTR.classList.add("hide");
+    }
+}
 
 
 
 
 $('#busqueda').keyup(function(e){
-     consulta = $("#busqueda").val();
-     $.ajax({
-         data: {'busca': consulta},
-         url: '/listadoControles/',
-         type: 'get',
-         success : function(data) {
-                 console.log(data[0].nombre);
-     },
-     error : function(message) {
-             console.log(message);
-          }
-     });
+    consulta = $("#busqueda").val();
+    $.ajax({
+        data: {'busca': consulta},
+        url: '/listadoControles/',
+        type: 'get',
+        success : function(data) {
+            console.log(data[0].nombre);
+        },
+        error : function(message) {
+            console.log(message);
+        }
+    });
 });
 
 $(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 
